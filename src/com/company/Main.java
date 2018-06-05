01efd63935dff52eeef76f601ca58c7cb1f273ae
Kamyah Rasheed <kamyah.rasheed@stu.jefferson.kyschools.us>
2018-06-05 13:41:42 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int var1 = 0;
+        char var2 = 'h';
+        String var3 = "Hello!";
+        boolean var4 = true;
+        double var5 = 8.9;
+
+        int age = 15;
+        char firstInitial = 'K';
+        String intro = "Good morning!";
+        boolean yesOrNo = true;
+        double gradePointAverage = 0.01;
+
+
+
+        System.out.println("Who are you?");
+
+        String hello = "Hello";
+        String name = "Stranger";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+
+        int age2 = input.nextInt();
+
+        System.out.println("You're " +age2 + ", hmmm?");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+
+        name = scan.nextLine();
+
+        System.out.println(hello + " " + name +"!");
+
+
+        //hmmmmmmmmmmmmm m m m m m m m m mmm?
+        // m m m m mmmmmmm m m m mmmm s he hung e r s
+
+    }
+}
